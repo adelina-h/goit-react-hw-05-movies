00dc84b47ec83a985db0c819a0a1f1a12ecb3c20
Adelina-hub <adelina.macarascu@gmail.com>
2025-02-16 23:37:33 +0400

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,25 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
-import {
-  Container,
-  Header,
-  NavList,
-  NavItem,
-  StyledNavLink,
-  Main,
-} from './AppStyled.jsx';
+import { lazy } from 'react';
+import { SharedLayout } from './SharedLayouts/SharedLayouts';
 
-const HomePage = lazy(() => import('pages/HomePage'));
-const MovieDetails = lazy(() => import('pages/MovieDetails'));
-const Movies = lazy(() => import('pages/Movies'));
-const Cast = lazy(() => import('./Cast'));
-const Reviews = lazy(() => import('./Reviews'));
+const Home = lazy(() => import('../pages/Home'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('../components/Cast/Cast'));
+const Reviews = lazy(() => import('../components/Reviews/Reviews'));
+const Movies = lazy(() => import('../pages/Movies'));
 
 export const App = () => {
   return (
-    <Suspense fallback={<div>Loading page...</div>}>
-      <Container>
-        <Header>
-          <NavList>
-            <NavItem>
-              <StyledNavLink to="/">Home</StyledNavLink>
-            </NavItem>
-            <NavItem>
-              <StyledNavLink to="/movies">Movies</StyledNavLink>
-            </NavItem>
-          </NavList>
-        </Header>
-
-        <Main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:movieId" element={<MovieDetails />}>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </Main>
-      </Container>
-    </Suspense>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SWRConfig } from 'swr';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import './index.css';
+import { GlobalStyles } from 'styles/GlobalStyles.styled';
+
+const fetcher = async (...args) => {
+  const res = await fetch(...args);
+  if (!res.ok) {
+    const error = new Error('An error occurred while fetching the data.');
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <SWRConfig value={{ fetcher }}>
+        <GlobalStyles />
+        <App />
+      </SWRConfig>
+    </BrowserRouter>
   </React.StrictMode>
 );
